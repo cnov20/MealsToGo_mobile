f38c59ae86f32769cad7d183162549b3b57a7c55
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-21 20:44:17 -0400

    Created and coded (5) files for insfrastructure/theme-focused code—scaffolding and settings to provide to Theme-Provider

---
+++ src/infrastructure/theme/spacing.js
@@ -0,0 +1,13 @@
+export const lineHeights = {
+    title: "28px",
+    copy: "20px",
+  };
+  
+  export const space = ["0px", "4px", "8px", "16px", "32px", "64px"];
+  © 2022 GitHub, Inc.
+  Terms
+  Privacy
+  Security
+  Status
+  Docs
+  

bc25820627e38e2ff1e782c405a6053f90a44258
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-22 12:00:16 -0400

    Re-factored / Optimized — Spacer — Component and addressed Spacer-component issue/bug (Android-only/Android-specific)

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -91,10 +91,12 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
                         </Rating>
                         <SectionEnd>
                             {isClosedTemporarily && <Closed variant="label">CLOSED TEMPORARILY</Closed>}
-                            <Spacer variant="left.large"></Spacer>
-                            {isOpenNow && <OpenIcon xml={open} width={20} height={20} />}
-                            <Spacer variant="left.large"></Spacer>
-                            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }}></Image>
+                            <Spacer position="left" size="large">
+                                {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+                            </Spacer>
+                            <Spacer position="left" size="large">
+                                <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+                             </Spacer>
                         </SectionEnd>     
                     </Section>
                     <Address>{address}</Address>

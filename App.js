6d8ef696dba10c3d6315c4b8a213ace283464bc0
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-23 18:32:53 -0400

    Added icons to bottom-tabs Navigator — first iteration

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import { ThemeProvider } from 'styled-components/native';
 import RestaurantsScreen from './src/features/restaurants/screens/restaurants.screen';
 import { SafeArea } from './src/components/utils/safe-area.component';
 
+import { Ionicons } from '@expo/vector-icons';
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -74,7 +76,27 @@ export default function App() {
       <>
         <ThemeProvider theme={theme}>
           <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+    
+                  if (route.name === 'Restaurants') {
+                    iconName = focused
+                      ? 'md-restaurant'
+                      : 'ios-information-circle-outline';
+                  } else if (route.name === 'Map') {
+                    iconName = focused ? 'md-map' : 'ios-list';
+                  } else if (route.name === 'Settings') {
+                    iconName = focused ? 'md-settings' : 'ios-list';
+                  }
+    
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}>
               <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
               <Tab.Screen name="Map" component={MapScreen} />
               <Tab.Screen name="Settings" component={SettingsScreen} />

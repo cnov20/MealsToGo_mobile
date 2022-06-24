5e5fb6a025268e03196f47eab0f2852cc857998c
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-24 12:49:42 -0400

    Set up state / useState hooks (3) — restaurant output data, loading status, error handling — to be used with Context API

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,14 +1,45 @@
-import react, { createContext, useEffect, useMemo } from 'react';
+import react, { createContext, useEffect, useMemo, useState } from 'react';
 
 import { restaurantsRequest, restaurantsTransform } from './restaurants.service';
 
 const RestaurantsContext = createContext();
 
 const RestaurantsContextProvider = function ({ children }) {
-    return <RestaurantsContext.Provider value={{
-        restaurants: [1, 2, 3, 4, 5]
-    }}>{children}</RestaurantsContext.Provider>;
-};
 
+    const [restaurants, setRestaurants] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const retrieveRestaurants = function () {
+        setIsLoading(true);
+        setTimeout(function () {
+            restaurantsRequest()
+                .then(restaurantsTransform)
+                .then((results) => {
+                    setIsLoading(false);
+                    setRestaurants(results);
+                }).catch(error => {
+                    setIsLoading(false);
+                    setError(error);
+                })
+        }, 2000)
+    };
+
+    useEffect(function () {
+        retrieveRestaurants();
+    }, []);
+
+    return (
+        <RestaurantsContext.Provider
+            value={{
+                restaurants: restaurants,
+                isLoading: isLoading,
+                error: error
+            }}
+        >
+            {children}
+        </RestaurantsContext.Provider>
+    );
+}
 
 export { RestaurantsContext, RestaurantsContextProvider };

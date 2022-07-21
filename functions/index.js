13c1fd10ce9c2d8556a294a8ed383d7bd481e8cb
Chris Novelli <chrisnovelli0@gmail.com>
2022-07-21 13:27:06 -0400

    Google Client / API — End-to-end feature(s) — now fully set-up and integrated — getting live location and geocoding information — just need to set this up for restaurant images (that's the last piece still being "mocked")

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require("firebase-functions");
 const {geocodeRequest} = require("./geocode");
-const {placesRequest} = require("./places");
+const { placesRequest } = require("./places");
+
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,9 +13,9 @@ exports.geocode = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!!!!!");
 
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
--- functions/places/index.js
+++ functions/places/index.js
@@ -1,14 +1,35 @@
 const {mocks, addMockImage} = require("./mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.placesRequest = (request, response) => {
-  const {location} = url.parse(request.url, true).query;
-  const data = mocks[location];
+module.exports.placesRequest = (request, response, client) => {
 
-  if (data) {
-    data.results = data.results.map(addMockImage);
+  const { location, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const data = mocks[location];
+    if (data) {
+      data.results = data.results.map(addMockImage);
+    }
+
+    // response.send(mocks[location]);
+    return response.json(data);
   }
-
-  // response.send(mocks[location]);
-  response.json(data);
+  client
+      .placesNearby({
+        params: {
+          location: location,
+          radius: 1500,
+          type: "restaurant",
+          key: functions.config().google.key,
+        },
+        timeout: 1000,
+      })
+      .then((res) => {
+        res.data.results = res.data.results.map(addMockImage);
+        return response.json(res.data);
+      })
+      .catch((e) => {
+        response.status(400);
+        return response.send(e.response.data.error_message);
+      });
 };
